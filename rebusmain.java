532e972b663a826310d0a356e2e2eb2857852a4a
Carle38 <carle38@gmail.com>
2012-02-04 16:12:37 -0500

    Update rebusmain.java

--- rebusmain.java
+++ rebusmain.java
@@ -2,12 +2,14 @@
  * Rebus generator
  * By Carl Estabrook
  * 8/23/2011
+ * program completes problem (rebus generator) from:
+ * http://www.itasoftware.com/careers/work-at-ita/hiring-puzzles.html
  */
 public class rebusmain 
 {
-	public static void main(String... aArgs) 
+	public static void main(String[] aArgs) 
 	{
 		rebusGUI gui = new rebusGUI();
 		gui.setVisible(true);    
-    }
+        }
 }
